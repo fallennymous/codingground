25df99d088552ae7b83d0d1aeb5f37876d28925d
fallennymous <fallennymous@gmail.com>
2017-02-27 10:42:01 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "Adam";
+        String kota = "Jepara";
+        System.out.println(nama + " " + kota);
+     }
+}
